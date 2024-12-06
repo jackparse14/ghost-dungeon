13c0486b156b7d68144f1317caa76c21243a0860
Jack Parsons <jackparse14@hotmail.co.uk>
2024-12-06 14:11:22 +0000

    Made splash screen manager work for multiple screens

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -15,6 +15,7 @@ public partial class GameController : Node
     public override void _Ready()
     {
         Global.gameController = this;
+        currentGuiScene = GetNode<Control>("GUI/SplashScreenManager");
     }
 
     public void Change3DScene(String newScene, bool delete = true, bool keepRunning = false)
--- Scripts/SplashScreenManager.cs
+++ Scripts/SplashScreenManager.cs
@@ -9,26 +9,54 @@ public partial class SplashScreenManager : Control
     [Export] float pauseTime = 1.5f;
     [Export] float fadeOutTime = 1.5f;
     [Export] float outTime = 0.5f;
-    [Export] TextureRect splashScreen;
+    //[Export] TextureRect splashScreen;
+
+    [Export] Node splashScreenContainer;
+
+    private Godot.Collections.Array<TextureRect> splashScreens;
 
     public override void _Ready()
     {
+        getScreens();
         fade();
     }
+
+    public void getScreens()
+    {
+        splashScreens = new Godot.Collections.Array<TextureRect>();
+        foreach(TextureRect screen in splashScreenContainer.GetChildren())
+        {
+            splashScreens.Add(screen);
+            screen.Modulate = new Color(screen.Modulate.R, screen.Modulate.G, screen.Modulate.B, 0.0f);
+        }
+    }
+
     public async void fade() {
-        splashScreen.Modulate = new Color(splashScreen.Modulate.R, splashScreen.Modulate.G, splashScreen.Modulate.B, 0.0f);
-        Tween tween = CreateTween();
-        tween.TweenProperty(splashScreen, "modulate:a", 1.0f, fadeInTime);
+        foreach (TextureRect screen in splashScreens)
+        {
+            // Fade In
+            Tween tween = GetTree().CreateTween();
+            tween.TweenProperty(screen, "modulate:a", 1.0f, fadeInTime);
+            await ToSignal(tween, "finished");
+            
+            // Pause
+            await ToSignal(GetTree().CreateTimer(pauseTime), "timeout");
+            
+            // Fade Out
+            tween = GetTree().CreateTween();
+            tween.TweenProperty(screen, "modulate:a", 0.0f, fadeOutTime);
+            await ToSignal(tween, "finished");
 
-        await ToSignal(tween, "tween_all_completed");
-        await ToSignal(GetTree().CreateTimer(pauseTime), "timeout");
-
-        tween.TweenProperty(splashScreen, "modulate:a", 0.0f, fadeOutTime);
-
-        await ToSignal(tween,"tween_all_completed");
-        await ToSignal(GetTree().CreateTimer(outTime), "timeout");
-
+            // Pause
+            await ToSignal(GetTree().CreateTimer(outTime), "timeout");
+        }
         Global.gameController.ChangeGuiScene("res://Scenes/MainMenu.tscn");
     }
 
+
+    public void unhandledInput(InputEvent input){
+        if (input.IsPressed()) {
+            Global.gameController.ChangeGuiScene("res://Scenes/MainMenu.tscn");
+        }
+    }
 }
